134eab74e7808c653f0b00b01d9345ee2b8ce80c
Morryty <rmotyrev@list.ru>
2025-07-02 22:00:10 +0500

    Prototype/sketch

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,122 +1,170 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(CybertrixApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class CybertrixApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Cybertrix Lab',
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        '/portfolio': (context) => PortfolioPage(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      appBar: AppBar(title: Text("Cybertrix Lab")),
+      body: SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Placeholder(fallbackHeight: 150, fallbackWidth: 150),
+                SizedBox(width: 20),
+                Text(
+                  "Cybertrix Lab\nОбщее описание лаборатории",
+                  style: TextStyle(fontSize: 18),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+
+            // Направления
+            Text("Направления деятельности", style: TextStyle(fontSize: 20)),
+            DirectionBlock(
+              title: "Направление 1",
+              description: "Описание направления 3",
+            ),
+
+            DirectionBlock(
+              title: "Направление 2",
+              description: "Описание направления 3",
+            ),
+
+            DirectionBlock(
+              title: "Направление 3",
+              description: "Описание направления 3",
+            ),
+
+            SizedBox(height: 20),
+
+            // Применение технологий
+            Container(
+              padding: EdgeInsets.all(20),
+              color: Colors.green[100],
+              child: Text("Применение наших технологий"),
+            ),
+
+            SizedBox(height: 20),
+
+            // Карусель проектов
+            Container(
+              height: 200,
+              color: Colors.yellow[100],
+              child: PageView(
+                children: [
+                  ProjectCard(title: "Проект 1"),
+                  ProjectCard(title: "Проект 2"),
+                  ProjectCard(title: "Проект 3"),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            // Контакты
+            Container(
+              color: Colors.purple[100],
+              padding: EdgeInsets.all(16),
+              child: Text("Контактная информация"),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class DirectionBlock extends StatelessWidget {
+  final String title;
+  final String? description;
+
+  const DirectionBlock({required this.title, this.description});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 24),
+      child: ListTile(
+        title: Text(title),
+        subtitle: description != null ? Text(description!) : null,
+      ),
+    );
+  }
+}
+
+class ProjectCard extends StatelessWidget {
+  final String title;
+
+  const ProjectCard({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text(title, style: TextStyle(fontSize: 18)));
+  }
+}
+
+class PortfolioPage extends StatelessWidget {
+  final List<Map<String, String>> projects = [
+    {'title': 'Проект 1', 'desc': 'Описание проекта 1'},
+    {'title': 'Проект 2', 'desc': 'Описание проекта 2'},
+    {'title': 'Проект 3', 'desc': 'Описание проекта 3'},
+    {'title': 'Проект 4', 'desc': 'Описание проекта 4'},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Портфолио")),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: GridView.count(
+          crossAxisCount: 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
+          children: projects.map((proj) {
+            return Container(
+              padding: EdgeInsets.all(12),
+              color: Colors.grey[200],
+              child: Column(
+                children: [
+                  Placeholder(fallbackHeight: 100),
+                  SizedBox(height: 10),
+                  Text(
+                    proj['title']!,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 5),
+                  Text(proj['desc']!, textAlign: TextAlign.center),
+                  SizedBox(height: 10),
+                  ElevatedButton(onPressed: () {}, child: Text("Подробнее")),
+                ],
+              ),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
